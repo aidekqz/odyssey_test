54ec4b21ca2a4693f4496ed1b1aed178122e2a03
Yaroslav Chirkov <aidekqz@gmail.com>
2024-04-01 14:19:24 +0300

    update run.sh and ignore odyssey bin

--- run.sh
+++ run.sh
@@ -8,9 +8,13 @@ sleep 5
 docker exec -it postgres bash -c "sed -i 's/scram-sha-256/md5/g' /var/lib/postgresql/data/pg_hba.conf"
 sleep 2
 psql "host=127.0.0.1 port=5432 user=postgres password=123456 dbname=postgres" -f init.sql
+
+pkill odyssey 2>/dev/null || true
+
 ./odyssey ./odyssey.conf
 pgrep odyssey
 psql "host=127.0.0.1 port=6432 user=postgres password=123456 dbname=postgres" -c "SELECT 1" && echo "OK"
 kill -s HUP $(pgrep odyssey)
 psql "host=127.0.0.1 port=6432 user=postgres password=123456 dbname=postgres" -c "SELECT 1" || echo "Failed"
 ls -alht /var/lib/apport/coredump/*
+pgrep odyssey && echo "Good"
